2c0da5a1a46a086ced606f20f28937e30a2faa2b
Saul Field <saul.field@gmail.com>
2025-07-09 16:48:01 -0400

    Refactor bytecode compiler to generate list of instructions rather than emitting directly

--- olox/bin/compiler.ml
+++ olox/bin/compiler.ml
@@ -2,7 +2,7 @@ open Printf
 open Olox
 open Ast
 
-type opcode =
+type bytecode_op =
   | OP_CONSTANT of float
   | OP_NIL
   | OP_TRUE
@@ -19,15 +19,15 @@ type opcode =
   | OP_RETURN
 [@@deriving show]
 
-let string_of_opcode binop =
-  match binop with
+let string_of_op op =
+  match op with
   | OP_CONSTANT f -> Printf.sprintf "CONST %g" f
   | OP_NIL -> "PUSH_NIL"
   | OP_TRUE -> "PUSH_TRUE"
   | OP_FALSE -> "PUSH_FALSE"
-  | OP_EQUAL -> "EQUAL"
-  | OP_GREATER -> "GREATER"
-  | OP_LESS -> "LESS"
+  | OP_EQUAL -> "EQ"
+  | OP_GREATER -> "GT"
+  | OP_LESS -> "LT"
   | OP_ADD -> "ADD"
   | OP_SUB -> "SUB"
   | OP_MUL -> "MUL"
@@ -48,70 +48,64 @@ let parse src =
   if !Common.had_error = false then Resolve.run stmts |> ignore;
   stmts
 
-let emit_const ch lit =
-  let opcode =
-    match lit with
-    | LIT_number n -> OP_CONSTANT n
-    | LIT_bool true -> OP_TRUE
-    | LIT_bool false -> OP_FALSE
-    | LIT_nil -> OP_NIL
-    | LIT_string _s -> failwith "Strings not yet implemented."
-  in
-  fprintf ch "%s\n" (string_of_opcode opcode)
+let compile_literal lit =
+  match lit with
+  | LIT_number n -> OP_CONSTANT n
+  | LIT_bool true -> OP_TRUE
+  | LIT_bool false -> OP_FALSE
+  | LIT_nil -> OP_NIL
+  | LIT_string _s -> failwith "Strings not yet implemented."
 
-let emit_unary ch op =
-  let opcode =
-    match op with
-    | UNOP_neg -> OP_NEG
-    | UNOP_not -> OP_NOT
-  in
-  let op_str = string_of_opcode opcode in
-  fprintf ch "%s\n" op_str
+let compile_unary op =
+  match op with
+  | UNOP_neg -> OP_NEG
+  | UNOP_not -> OP_NOT
 
-let emit_binary ch op =
-  let opcode =
-    match op with
-    | BINOP_add -> OP_ADD
-    | BINOP_sub -> OP_SUB
-    | BINOP_mul -> OP_MUL
-    | BINOP_div -> OP_DIV
-    | BINOP_eq -> OP_EQUAL
-    | BINOP_gt -> OP_GREATER
-    | BINOP_lt -> OP_LESS
-    | BINOP_ne -> failwith ""
-    | BINOP_le -> failwith ""
-    | BINOP_ge -> failwith ""
-  in
-  let op_str = string_of_opcode opcode in
-  fprintf ch "%s\n" op_str
+let compile_binary op =
+  match op with
+  | BINOP_add -> [ OP_ADD ]
+  | BINOP_sub -> [ OP_SUB ]
+  | BINOP_mul -> [ OP_MUL ]
+  | BINOP_div -> [ OP_DIV ]
+  | BINOP_eq -> [ OP_EQUAL ]
+  | BINOP_gt -> [ OP_GREATER ]
+  | BINOP_lt -> [ OP_LESS ]
+  | BINOP_ne -> [ OP_EQUAL; OP_NOT ]
+  | BINOP_le -> [ OP_GREATER; OP_NOT ]
+  | BINOP_ge -> [ OP_LESS; OP_NOT ]
 
-let rec compile_expr ch expr =
+let rec compile_expr expr =
   match expr with
   (* | EXPR_Assign (var, expr) -> failwith "" *)
   | EXPR_Binary (lhs, op, rhs) ->
-      compile_expr ch lhs;
-      compile_expr ch rhs;
-      emit_binary ch op
+      compile_expr lhs @ compile_expr rhs @ compile_binary op
   (* | EXPR_Call (callee_expr, arg_exprs) -> failwith "" *)
-  | EXPR_Grouping inner -> compile_expr ch inner
+  | EXPR_Grouping inner -> compile_expr inner
   (* | EXPR_Logical (lhs, op, rhs) -> failwith "" *)
-  | EXPR_Literal lit -> emit_const ch lit
-  | EXPR_Unary (op, inner) ->
-      compile_expr ch inner;
-      emit_unary ch op
+  | EXPR_Literal lit -> [ compile_literal lit ]
+  | EXPR_Unary (op, inner) -> compile_expr inner @ [ compile_unary op ]
   (* | EXPR_Variable var -> failwith "" *)
   | _ -> failwith ("Unimplemented expr" ^ show_expr expr)
 
-let compile_stmt ch stmt =
+let compile_stmt stmt =
   match stmt with
-  | STMT_Print expr -> compile_expr ch expr
+  | STMT_Print expr -> compile_expr expr @ [ OP_RETURN ]
   | _ -> failwith ("Unimplemented stmt" ^ show_stmt stmt)
 
 let compile ast =
+  let rec step ops stmts =
+    match stmts with
+    | [] -> ops
+    | stmt :: rest -> ops @ step (compile_stmt stmt) rest
+  in
+  step [] ast
+
+let emit ch op = fprintf ch "%s\n" (string_of_op op)
+
+let emit_bytecode ops =
   let filename = "out.byte" in
   let ch = open_out filename in
-  List.iter (fun stmt -> compile_stmt ch stmt) ast;
-  fprintf ch "%s" (string_of_opcode OP_RETURN);
+  List.iter (fun op -> emit ch op) ops;
   close_out ch;
   print_endline ("Compiled to: " ^ filename)
 
@@ -119,7 +113,9 @@ let run filename =
   let src = read_file filename in
   let ast = parse src in
   (* List.iter (fun stmt -> print_endline (Ast.show_stmt stmt)) ast; *)
-  compile ast
+  let bytecode = compile ast in
+  List.iter (fun op -> print_endline (string_of_op op)) bytecode;
+  emit_bytecode bytecode
 
 let main args =
   match Array.length args with
