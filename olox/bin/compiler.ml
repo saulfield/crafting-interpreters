3ab34faa06cfe31e514a2de3936b27df4c0bfd56
Saul Field <saul.field@gmail.com>
2025-07-09 15:57:29 -0400

    Add remaining operations to bytecode compiler

--- olox/bin/compiler.ml
+++ olox/bin/compiler.ml
@@ -2,6 +2,40 @@ open Printf
 open Olox
 open Ast
 
+type opcode =
+  | OP_CONSTANT of float
+  | OP_NIL
+  | OP_TRUE
+  | OP_FALSE
+  | OP_EQUAL
+  | OP_GREATER
+  | OP_LESS
+  | OP_ADD
+  | OP_SUB
+  | OP_MUL
+  | OP_DIV
+  | OP_NOT
+  | OP_NEG
+  | OP_RETURN
+[@@deriving show]
+
+let string_of_opcode binop =
+  match binop with
+  | OP_CONSTANT f -> Printf.sprintf "CONST %g" f
+  | OP_NIL -> "PUSH_NIL"
+  | OP_TRUE -> "PUSH_TRUE"
+  | OP_FALSE -> "PUSH_FALSE"
+  | OP_EQUAL -> "EQUAL"
+  | OP_GREATER -> "GREATER"
+  | OP_LESS -> "LESS"
+  | OP_ADD -> "ADD"
+  | OP_SUB -> "SUB"
+  | OP_MUL -> "MUL"
+  | OP_DIV -> "DIV"
+  | OP_NOT -> "NOT"
+  | OP_NEG -> "NEG"
+  | OP_RETURN -> "RET"
+
 let read_file filename =
   let ch = open_in_bin filename in
   let src = really_input_string ch (in_channel_length ch) in
@@ -14,25 +48,41 @@ let parse src =
   if !Common.had_error = false then Resolve.run stmts |> ignore;
   stmts
 
-let emit_const ch lit = fprintf ch "CONST %s\n" (string_of_literal lit)
+let emit_const ch lit =
+  let opcode =
+    match lit with
+    | LIT_number n -> OP_CONSTANT n
+    | LIT_bool true -> OP_TRUE
+    | LIT_bool false -> OP_FALSE
+    | LIT_nil -> OP_NIL
+    | LIT_string _s -> failwith "Strings not yet implemented."
+  in
+  fprintf ch "%s\n" (string_of_opcode opcode)
 
 let emit_unary ch op =
-  let op_str =
+  let opcode =
     match op with
-    | UNOP_neg -> "NEG"
-    | _ -> failwith ("Unimplemented unary op: " ^ show_unop op)
+    | UNOP_neg -> OP_NEG
+    | UNOP_not -> OP_NOT
   in
+  let op_str = string_of_opcode opcode in
   fprintf ch "%s\n" op_str
 
 let emit_binary ch op =
-  let op_str =
+  let opcode =
     match op with
-    | BINOP_add -> "ADD"
-    | BINOP_sub -> "SUB"
-    | BINOP_mul -> "MUL"
-    | BINOP_div -> "DIV"
-    | _ -> failwith ("Unimplemented binary op: " ^ show_binop op)
+    | BINOP_add -> OP_ADD
+    | BINOP_sub -> OP_SUB
+    | BINOP_mul -> OP_MUL
+    | BINOP_div -> OP_DIV
+    | BINOP_eq -> OP_EQUAL
+    | BINOP_gt -> OP_GREATER
+    | BINOP_lt -> OP_LESS
+    | BINOP_ne -> failwith ""
+    | BINOP_le -> failwith ""
+    | BINOP_ge -> failwith ""
   in
+  let op_str = string_of_opcode opcode in
   fprintf ch "%s\n" op_str
 
 let rec compile_expr ch expr =
@@ -61,7 +111,7 @@ let compile ast =
   let filename = "out.byte" in
   let ch = open_out filename in
   List.iter (fun stmt -> compile_stmt ch stmt) ast;
-  fprintf ch "RET\n";
+  fprintf ch "%s" (string_of_opcode OP_RETURN);
   close_out ch;
   print_endline ("Compiled to: " ^ filename)
 
