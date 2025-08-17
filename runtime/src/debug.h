84292bacb30684484980a93a093675907c90a7c2
Saul Field <saul.field@gmail.com>
2025-08-17 15:33:43 -0400

    Remove old code

--- runtime/src/debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef lox_debug_h
-#define lox_debug_h
-
-#include "chunk.h"
-
-void disassemble_chunk(Chunk* chunk, const char* name);
-int disassemble_instruction(Chunk* chunk, int offset);
-
-#endif
--- zlox/README.md
+++
@@ -1,7 +0,0 @@
-# zlox (Zig)
-
-```sh
-zig run main.zig # Run REPL
-zig run main.zig -- ../tests/test.lox # Run file
-zig build-exe main.zig -O ReleaseSafe -fstrip -fsingle-threaded
-```
